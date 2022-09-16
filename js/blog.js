8524ceeca55ddb2dbfdd8c2a10b5b690d905b121
Your Name <samsulr99@gmail.com>
2022-09-16 11:31:51 +0700

    day 5

--- js/blog.js
+++ js/blog.js
@@ -1,123 +1,68 @@
-// let namaSiswa1 = "Bagus"
-// let namaSiswa2 = "Rizal"
-// let namaSiswa3 = "Khalid"
 
-// console.log(namaSiswa1);
-// console.log(namaSiswa2);
-// console.log(namaSiswa3);
+let dataBlog = [] // untuk menyimpan data-data blog
 
-// // Array
-// let namaSiswa = ["Bagus", "Rizal", "Khalid", 2022, true]
-// console.log(namaSiswa);
-// console.log(namaSiswa[1]);
-// console.log(namaSiswa[2]);
-
-// Object
-// let nama = "Bagus"
-// let alamat = "Tangerang"
-// let umur = 20
-
-// console.log(nama);
-// console.log(alamat);
-// console.log(umur);
-
-// let dataPersonal1 = {
-//     nama: "Bagus",
-//     alamat: "Tangerang",
-//     umur: 20,
-// }
-
-// let dataPersonal2 = {
-//     nama: "Rizal",
-//     alamat: "Jakarta",
-//     umur: 20,
-// }
-// let dataPersonal3 = {
-//     nama: "Rizal",
-//     alamat: "Jakarta",
-//     umur: 20,
-// }
-
-// let dataPersonal4 = {
-//     nama: "Rizal",
-//     alamat: "Jakarta",
-//     umur: 20,
-// }
-
-// console.log(dataPersonal1);
-// console.log(dataPersonal1.alamat);
-
-// Array Object
-// let dataPersonal = [
-//     {
-//         nama: "Aziz",
-//         alamat: "Jakarta"
-//     },
-//     {
-//         nama: "Rizal",
-//         alamat: "Jakarta",
-//     },
-//     {
-//         nama: "Khalid",
-//         alamat: "Jakarta",
-//     }
-// ]
-
-// console.log(dataPersonal);
-
-// let data = []
-
-// function addData(){
-
-//     let person = {
-//         name: "Samsul",
-//         address: "Tangerang"
-//     }
-    
-//     data.push(person)
-//     console.log(data);
-// }
-
-// let data = []
-
-// function addData(){
-
-//     let blog = {
-//         title: document.getElementById("input-blog-title").value,
-//         content: document.getElementById("input-blog-content").value
-//     }
-
-//     data.push(blog)
-//     console.log(data);
-// }
-
-
-let dataBlog = []
 function addBlog(event) {
-    event.preventDefault()
+    event.preventDefault() // agar tidak melakukan referesh halaman
 
     let title = document.getElementById("input-blog-title").value
     let content = document.getElementById("input-blog-content").value
+    // let startDate = document.getElementById("start-date").value
+    // let endDate = document.getElementById("end-date").value
+
+    // karena kita ingin mengambil file datanya bukan valuenya
     let image = document.getElementById("input-blog-image").files
 
-    // untuk membuat url gambar, agar tampil
-    image = URL.createObjectURL(image[0])
-    console.log(image);
+    let facebook = document.getElementById("input-fb").checked
+    let instagram = document.getElementById("input-ig").checked
+    let tiktok = document.getElementById("input-tok").checked
 
+    if(facebook){
+        facebook = document.getElementById("input-fb").value
+    } else {
+        facebook = ""
+    }
+
+    if(instagram){
+        instagram = document.getElementById("input-ig").value
+    } else {
+        instagram = ""
+    }
+
+    if(tiktok){
+        tiktok = document.getElementById("input-tok").value
+    } else {
+        tiktok = ""
+    }
+
+    // jika image diisi
+    if(image.length != 0){
+        // untuk membuat url gambar, agar tampil
+        image = URL.createObjectURL(image[0])
+    } else {
+        // jika tidak diisi
+        return alert("gambar wajib diisi!!")
+    }
+
+
+    // mengelompakan data menjadi object
     let blog = {
         title,
         content,
         image,
-        postAt: "15 September 2022",
-        author: "Samsul Rijal"
+        postAt: new Date(),
+        author: "Samsul Rijal",
+        // startDate,
+        // endDate,
+        facebook,
+        instagram,
+        tiktok
     }
 
     dataBlog.push(blog)
     console.log(dataBlog);
 
-    renderBlog()
+    renderBlog() // menjalankan function renderBlog agar data blog tampil
 }
-
 
 function renderBlog(){
 
@@ -143,30 +88,86 @@ function renderBlog(){
                     >
                 </h1>
                 <div class="detail-blog-content">
-                    ${dataBlog[index].postAt} | ${dataBlog[index].author}
+                    ${getFullTime(dataBlog[index].postAt)} | ${dataBlog[index].author}
                 </div>
                 <p>
                     ${dataBlog[index].content}
                 </p>
+                <i class="fa-brands fa-${dataBlog[index].facebook}"></i>
+                <i class="fa-brands fa-${dataBlog[index].instagram}"></i>
+                <i class="fa-brands fa-${dataBlog[index].tiktok}"></i>
+
+                <div style="float:right; margin: 10px">
+                    <p style="font-size: 15px; color:grey">${getDistanceTime(dataBlog[index].postAt)}</p>
+                </div>
                 </div>
             </div>
         `
     }
 }
 
-
-
-// document.getElementById("card").innerHTML = ''
-
-// for (let index = 0; index < dataBlog.length;  index++){  
-//     console.log(dataBlog[index]);
-
-//     document.getElementById("card").innerHTML += `
+function getFullTime(time){
     
-//     <div style="width: 400px; margin: 10px;">
-//         <img src="${dataBlog[index].image}" alt="" width="100%">
-//         <h2>${dataBlog[index].title}</h2>
-//     </div>
-//     `
-// }
-// }
+    // let time = new Date()
+    // console.log(time);
+    
+    let monthName = ['Jan', 'Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nop','Dec']
+    // console.log(monthName[8]);
+
+    let date = time.getDate()
+    console.log(date);
+
+    let monthIndex = time.getMonth()
+    console.log(monthIndex);
+
+    let year = time.getFullYear()
+    console.log(year);
+
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+
+    if(hours <= 9){
+        hours = "0" + hours
+    } else if(minutes <= 9){
+        minutes = "0" + minutes
+    }
+
+    // 16 Sep 2022 09:11 WIB
+    return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`
+}
+
+function getDistanceTime(time){
+
+    let timeNow = new Date()
+    let timePost = time
+
+    // waktu sekarang - waktu post
+    let distance = timeNow - timePost // hasilnya milisecond
+    console.log(distance);
+
+    let milisecond = 1000 // milisecond
+    let secondInHours = 3600 // 1 jam 3600 detik
+    let hoursInDay = 24 // 1 hari 24 jam
+
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+    let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
+    let distanceMinutes = Math.floor(distance / (milisecond * 60))
+    let distanceSecond = Math.floor(distance / milisecond)
+    
+    
+    if(distanceDay > 0){
+        return `${distanceDay} day ago`
+    } else if(distanceHours > 0){
+        return `${distanceHours} hours ago`
+    } else if(distanceMinutes > 0){
+        return `${distanceMinutes} minutes ago`
+    } else {
+        return `${distanceSecond} seconds ago`
+    }
+    
+}
+
+// setInterval(function(){
+//     renderBlog()
+// }, 3000)
