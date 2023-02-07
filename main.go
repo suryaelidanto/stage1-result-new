ef9363b62e1e14299d2245e81615219f971cf838
suryaa6666 <suryaelidanto@gmail.com>
2023-02-07 10:33:18 +0700

    result

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 	"text/template"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -17,6 +18,28 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+type Blog struct {
+	Title    string
+	Content  string
+	Author   string
+	PostDate string
+}
+
+var dataBlog = []Blog{
+	{
+		Title:    "Hallo Title",
+		Content:  "Hallo Content",
+		Author:   "Surya Elidanto",
+		PostDate: "07/02/2023",
+	},
+	{
+		Title:    "Hallo Title 2",
+		Content:  "Hallo Content 2",
+		Author:   "Surya Elidanto",
+		PostDate: "08/02/2023",
+	},
+}
+
 func main() {
 	// Create new Echo instance
 	e := echo.New()
@@ -25,7 +48,7 @@ func main() {
 	// e.Use(middleware.Logger())
 	// e.Use(middleware.Recover())
 
-	// Serve static files from "public" directory
+	// Serve static files from "/public" directory
 	e.Static("/public", "public")
 
 	t := &Template{
@@ -41,6 +64,7 @@ func main() {
 	e.GET("/blog", blog)
 	e.GET("/blog-detail/:id", blogDetail)
 	e.GET("/form-blog", formAddBlog)
+	e.GET("/blog-delete/:id", deleteBlog)
 	e.POST("/add-blog", addBlog)
 
 	// Start server
@@ -61,16 +85,31 @@ func contact(c echo.Context) error {
 }
 
 func blog(c echo.Context) error {
-	return c.Render(http.StatusOK, "blog.html", nil)
+	blogs := map[string]interface{}{
+		"Blogs": dataBlog,
+	}
+
+	return c.Render(http.StatusOK, "blog.html", blogs)
 }
 
 func blogDetail(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
+	var BlogDetail = Blog{}
+
+	for i, data := range dataBlog {
+		if id == i {
+			BlogDetail = Blog{
+				Title:    data.Title,
+				Content:  data.Content,
+				PostDate: data.PostDate,
+				Author:   data.Author,
+			}
+		}
+	}
+
 	data := map[string]interface{}{
-		"Id":      id,
-		"Title":   "Pasar Coding di Indonesia Dinilai Masih Menjanjikan",
-		"Content": "REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup.REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan pen...",
+		"Blog": BlogDetail,
 	}
 
 	return c.Render(http.StatusOK, "blog-detail.html", data)
@@ -87,5 +126,22 @@ func addBlog(c echo.Context) error {
 	println("Title : " + title)
 	println("Content : " + content)
 
+	var newBlog = Blog{
+		Title:    title,
+		Content:  content,
+		Author:   "Surya Elidanto",
+		PostDate: time.Now().String(),
+	}
+
+	dataBlog = append(dataBlog, newBlog)
+
 	return c.Redirect(http.StatusMovedPermanently, "/blog")
+}
+
+func deleteBlog(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	dataBlog = append(dataBlog[:id], dataBlog[id+1:]...)
+
+	return c.Redirect(http.StatusFound, "/blog")
 }
